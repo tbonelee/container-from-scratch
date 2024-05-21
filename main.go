d332fc1f0719f84a5c62e42278953fe54d7e457f
chanlee <chanlee@ps-analytics.com>
2024-05-21 23:27:13 +0900

    cgroup 격리

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"os/exec"
 	"syscall"
+	"path/filepath"
+	"io/ioutil"
 )
 
 func main() {
@@ -36,6 +38,8 @@ func run() {
 func child() {
 	fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
 
+	cg()
+
 	syscall.Sethostname([]byte("container")) // 새로운 namespace에 들어왔으므로 hostname을 변경해도 host의 hostname은 변경되지 않음
 	syscall.Chroot("/home/ubuntu/container") // chroot를 통해 root directory를 변경
 	syscall.Chdir("/")
@@ -47,6 +51,19 @@ func child() {
 	cmd.Stderr = os.Stderr
 	must(cmd.Run())
 
+}
+
+func cg() {
+	cgroups := "/sys/fs/cgroup/"
+	pids := filepath.Join(cgroups, "pids")
+	err := os.Mkdir(filepath.Join(pids, "container"), 0755) // 0755: rwxr-xr-x
+	if err != nil && !os.IsExist(err) {
+		panic(err)
+	}
+	must(ioutil.WriteFile(filepath.Join(pids, "container/pids.max"), []byte("20"), 0700)) // 0700: rwx------
+	// Removes the new cgroup in plcae after the container exits
+	must(ioutil.WriteFile(filepath.Join(pids, "container/notify_on_release"), []byte("1"), 0700))
+	must(ioutil.WriteFile(filepath.Join(pids, "container/cgroup.procs"), []byte(fmt.Sprintf("%d", os.Getpid())), 0700))
 }
 
 func must(err error) {
